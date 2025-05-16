9348f560194db4b656a27234d18c365e14460532
novlan1 <1576271227@qq.com>
2025-05-16 14:53:12 +0800

    chore: update vite.config

---
+++ script/export-default-in-vue.js
@@ -0,0 +1,3 @@
+const { exportDefaultInVue } = require('@plugin-light/vite-plugin-export-default-in-vue');
+
+exportDefaultInVue({ root: process.cwd() });
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 
-import { aliasForLibrary } from '@plugin-light/vite-plugin-alias-for-library';
+// import { aliasForLibrary } from '@plugin-light/vite-plugin-alias-for-library';
 import { genVersionWebVitePlugin } from '@plugin-light/vite-plugin-gen-version';
 import { ifdefVitePlugin } from '@plugin-light/vite-plugin-ifdef';
 
@@ -40,12 +40,12 @@ export default defineConfig({
       context: { H5: true, VUE3: true, VUE2: false },
       type: ['css', 'js', 'html'],
     }) as Plugin,
-    aliasForLibrary({
-      list: [
-        'press-ui',
-      ],
-      target: 'src/library',
-    }),
+    // aliasForLibrary({
+    //   list: [
+    //     'press-ui',
+    //   ],
+    //   target: 'src/library',
+    // }),
     genVersionWebVitePlugin({
       buildName: BUILD_NAME_MAP.build,
       commitName: BUILD_NAME_MAP.commit,
@@ -56,7 +56,7 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
       src: path.resolve(__dirname, 'src'),
-      'press-ui': path.resolve(__dirname, 'src/library/press-ui'),
+      // 'press-ui': path.resolve(__dirname, 'src/library/press-ui'),
     },
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
   },
@@ -70,7 +70,7 @@ export default defineConfig({
   },
   css: {
     preprocessorOptions: {
-      api: 'modern',
+      // api: 'modern',
       scss: {
         // https://sass-lang.com/documentation/breaking-changes/legacy-js-api/
         // https://sass-lang.com/documentation/breaking-changes/import/
