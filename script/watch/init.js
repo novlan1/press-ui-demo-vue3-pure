0884a62b204c76359265b913d5f2468f36a260d5
novlan1 <1576271227@qq.com>
2025-05-31 21:31:24 +0800

    chore: update press-ui@3.0.17

--- script/watch/init.js
+++ script/watch/init.js
@@ -3,7 +3,10 @@ const { initWithPressUI } = require('../../src/press-ui/script/watch/init');
 // const {replaceToVue3 } = require('../../src/press-ui/script/watch/vue3')
 
 function main() {
-  initWithPressUI();
+  initWithPressUI([
+    'pages',
+    'utils',
+  ]);
   // execCommand('rm -rf src/packages/press-hor-swiper-light', process.cwd(), 'inherit');
 
   // replaceToVue3()
